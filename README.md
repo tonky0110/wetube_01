7f95f16f843db39f1c60421b5bc14ef6041cbd6c
tonky0110 <tonky0110@gmail.com>
2018-12-15 23:33:52 +0900

    2.9 routers

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ morgan: logging middleware
 helmet: 보안 middleware
 cookie-parser:
 body-parser:
+
+
+export /export default 차이
+import할 때 
+  export: import {ex)router} from './router';
+  export default: import router from './router';
+
+
+MVC
+M: data
+V: how does the data look
+C: Function that looks for the data
--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import  globalRouter from "./routers/globalRouter";
+import  userRouter from "./routers/userRouter";
+import  videoRouter from "./routers/videoRouter";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from my home~");
-
-const handleProfile = (req, res) => res.send("Your are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,9 +15,8 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 export default app;

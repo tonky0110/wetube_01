2138868c34f24a437e92094d0b90b5b1c3c005f6
tonky0110 <tonky0110@gmail.com>
2018-12-15 20:43:41 +0900

    2.4 setting express server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hello from Home");
+}
+
+function handleProfile(req, res) {
+  res.send("Your are on my profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);

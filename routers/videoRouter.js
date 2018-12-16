a15ab6c6bda0d363a27694e7f2db7cc8ec4711c4
tonky0110 <tonky0110@gmail.com>
2018-12-16 22:37:09 +0900

    2.25 more controller done

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,14 +3,15 @@ import routes from "../routes";
 import {
   deleteVideo,
   editVideo,
-  upload,
-  videos,
-  videoDetail
+  getUpload,
+  videoDetail,
+  postUpload
 } from "../controller/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 

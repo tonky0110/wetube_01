afaf3adeca6c44e9fde3aa3e064cf6e8dbd510db
tonky0110 <tonky0110@gmail.com>
2018-12-16 17:42:47 +0900

    2.16 setting localMiddleware & send local variable

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 import express from 'express';
-import morgan from 'morgan';
 import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 import bodyParser from "body-parser";
-import  globalRouter from "./routers/globalRouter";
-import  userRouter from "./routers/userRouter";
-import  videoRouter from "./routers/videoRouter";
+import cookieParser from 'cookie-parser';
+import { localsMiddleware } from "./middlewares";
 import routes from './routes';
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 const app = express();
 
-app.set("view engine", "pug");
-
 // Add small middleware
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);

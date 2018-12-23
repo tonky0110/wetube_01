889a4d2819ee8964c2bda02530a8a66e95eb6ba5
tonky0110 <tonky0110@gmail.com>
2018-12-23 18:49:42 +0900

    3.9 update edit video detail

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,20 +1,29 @@
 import express from 'express';
 import routes from "../routes";
 import {
-  deleteVideo,
-  editVideo,
   getUpload,
+  postUpload,
   videoDetail,
-  postUpload
+  deleteVideo,
+  getEditVideo,
+  postEditVideo
 } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Video Edit
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Video Delete
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
